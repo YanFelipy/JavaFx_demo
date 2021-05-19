0bdac917e4035ac64424d6c4086dad13d5a34126
YanFelipy22 <yan-felipy@hotmail.com>
2021-05-19 19:19:21 -0300

    Aplicação demo finalizada

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import java.io.IOException;
+import java.util.Locale;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		Locale.setDefault(Locale.US);
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+	    		Scene scene = new Scene(parent);
+	        	stage.setScene(scene);
+	        	stage.show();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/ModulePath.java
@@ -0,0 +1,6 @@
+package application;
+
+public class ModulePath {
+
+	// --module-path C:\java-libs\javafx-sdk\lib --add-modules=javafx.fxml,javafx.controls
+}
